9d3540a6fb4242fd6d0a99175e7b4e0b7a393a36
kamir222 <katia.amir@gmail.com>
2017-07-05 23:32:52 +0000

    implement admin categories feature

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,27 @@
 class Admin::CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    # binding.pry
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
 end
