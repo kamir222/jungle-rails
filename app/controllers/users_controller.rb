5dbec15109bea0edee1d63a8437b5a6792b0f02e
kamir222 <katia.amir@gmail.com>
2017-07-06 17:32:57 +0000

    create users database and add a create route and save user sign up information in db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,30 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+    puts params.inspect
+    @user = User.new(user_params)
+
+
+    if @user.save
+      puts User.all
+      redirect_to root_path, notice: 'User created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password_digest
+    )
   end
 end
